a7e78da98b07a53e23ed5ca82740e72fcb6269d2
ndukanice <ndukanice@gmail.com>
2026-03-23 03:31:56 +0100

    Add organization details page with related project queries

--- src/controllers/routes.js
+++ src/controllers/routes.js
@@ -1,7 +1,7 @@
 import express from 'express';
 
 import { showHomePage } from './index.js';
-import { showOrganizationsPage } from './organizations.js';
+import { showOrganizationDetailsPage, showOrganizationsPage } from './organizations.js';
 import { showProjectsPage } from './projects.js';
 import { showCategoriesPage } from './categories.js';
 import { testErrorPage } from './errors.js';
@@ -12,6 +12,7 @@ router.get('/', showHomePage);
 router.get('/organizations', showOrganizationsPage);
 router.get('/projects', showProjectsPage);
 router.get('/categories', showCategoriesPage);
+router.get('/organization/:id', showOrganizationDetailsPage);
 
 // error-handling routes
 router.get('/test-error', testErrorPage);
